2860a6499b555fbe9ce2e28d0df6d6f6abbd199e
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2017-03-01 09:40:03 +0100

    replace done. Late on planning.

--- model/file.php
+++ model/file.php
@@ -21,39 +21,6 @@ function replace_file($pathOldFile, $file){
     if (!move_uploaded_file($file["tmp_name"], $pathOldFile)){
         $_SESSION["errorMessage"] = "your file wasn't uploaded. Please try seeing if your username is a valid one.";
     }
-    /*$_SESSION["errorMessage"] = "";
-        $file = $_FILES["file"];
-
-
-        $oldPathFile = $informationsFile["pathFile"];
-
-        preg_match('/\.[0-9a-z]+$/', $file["name"], $cor);
-
-        $type = $cor[0];
-
-        $nameFile = $informationsFile["nameFile"];
-
-        $pathFile = "../../../files/".$_SESSION["username"]."/".$nameFile.$type;
-
-
-        if ($oldPathFile != $pathFile) {
-            $_SESSION["errorMessage"] = "The file you try to replace does not exist in database. (Check wether the type of the file you wish to replace is the same as the one you wish to upload instead)";
-        }elseif(empty($_FILES['file']['name'])){
-            $_SESSION["errorMessage"] = "You must choose a file to upload.";
-        }
-
-
-
-
-
-        if ($_SESSION["errorMessage"] == "") {
-            $pathFile = "../../../files/".$_SESSION["username"]."/".$nameFile;
-
-
-            if (!move_uploaded_file($file["tmp_name"], $pathFile)){
-                $_SESSION["errorMessage"] = "your file wasn't uploaded. Please try seeing if your username is a valid one.";
-            }
-        }*/
 }
 
 function download_file($fileData){
